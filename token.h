aeef571291cdeed22b56f776e429dc9a6ab9a1c6
uvsqNathanAmoussou <nathan.amoussou@ens.uvsq.fr>
2024-01-04 17:50:46 +0100

    Question 1 faite. Création fichier token.h pour répondre à la question 1.

---
+++ token.h
@@ -0,0 +1,14 @@
+// Question 1
+
+#define SIGMA 101  // Σ, contient toutes les lettres minuscules de a à z
+#define EPSILON 102  // ε, noté E
+#define EMPTY_SET 103  // ∅, noté O
+
+// Opérateurs
+#define PLUS 201  // +
+#define DOT 202  // .
+#define STAR 203  // *
+
+// Parenthèses
+#define PAR_O 301  // (
+#define PAR_F 302  // )
