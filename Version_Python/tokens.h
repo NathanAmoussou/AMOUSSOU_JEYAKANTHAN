d55ab63cfaeacc2c469e36fb8b4cfb736fadad79
uvsqNathanAmoussou <nathan.amoussou@ens.uvsq.fr>
2024-01-06 11:52:13 +0100

    Question 3 faite. Description de la progression dans reponses.txt.

---
+++ Version_Python/tokens.h
@@ -0,0 +1,16 @@
+// Question 1
+
+#ifndef TOKENS_H
+#define TOKENS_H
+
+// Définition des tokens
+#define PAR_O 101       // Parenthèse ouvrante '('
+#define PAR_F 102       // Parenthèse fermante ')'
+#define PLUS 103        // Opérateur '+'
+#define DOT 104         // Opérateur '.'
+#define STAR 105        // Opérateur '*'
+#define EPSILON 106     // Epsilon 'E'
+#define EMPTY_SET 107   // Ensemble vide 'O'
+#define LETTER 108      // Lettres minuscules de 'a' à 'z'
+
+#endif
